14ebff703d9a3fa8d890b74a8eb83dd2a124ac12
sergio <sergio.frascoza@gmail.com>
2024-09-26 14:58:31 +0200

    newRun

--- SRGoalSlice.py
+++ SRGoalSlice.py
@@ -397,7 +397,7 @@ def get_goal_sequence(total_episodes, goal_size):
 # parameters for training
 train_episode_length = 100
 test_episode_length = 100
-episodes = 100000
+episodes = 200000
 gamma = 0.8
 lr = 0.01
 
